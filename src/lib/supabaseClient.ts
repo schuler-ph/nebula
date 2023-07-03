0faba77ead57e6cd37e82e9763d8c30bd7cfee9d
Philipp Schuler <schulerp03@gmail.com>
2023-07-03 17:29:45 +0200

    new entry shenanigans

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -4,3 +4,12 @@ export const supabase = createClient(
   "https://nsysvbhfzbomwglmhzus.supabase.co",
   import.meta.env.VITE_SUPABASE_KEY
 );
+
+export const getUserId = async () => {
+  const { data, error } = await supabase.auth.getUser();
+  if (error === null) {
+    return data.user.id;
+  } else {
+    return "";
+  }
+};
--- src/views/diary/NewEntry.vue
+++ src/views/diary/NewEntry.vue
@@ -1,5 +1,24 @@
 <template>
-  <div>sdfiuh</div>
+  <v-form>
+    <!-- <v-text-field v-model="entry?.title" label="Title"></v-text-field> -->
+  </v-form>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+import { InsertDto } from "@/types/supabaseHelper";
+import { getUserId } from "@/lib/supabaseClient";
+import { onMounted } from "vue";
+
+const entry = ref<InsertDto<"diary">>();
+
+onMounted(async () => {
+  entry.value = {
+    user_id: await getUserId(),
+    day: new Date().toISOString().substring(0, 10),
+    title: "",
+    content: "",
+  };
+  console.log(entry.value);
+});
+</script>
