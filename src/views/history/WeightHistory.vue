c0d65557587783ceef2a35814c411ce1db3bb32a
Philipp Schuler <schulerp03@gmail.com>
2023-09-05 19:46:58 +0200

    0.2.12 gave up weight history

--- src/views/history/WeightHistory.vue
+++ src/views/history/WeightHistory.vue
@@ -1,67 +1,7 @@
 <template>
-  <Line v-if="loadingDataFinished" :data="plotData" :options="plotOptions" />
+  <div></div>
 </template>
 
-<script setup lang="ts">
-import { supabase } from "@/lib/supabaseClient";
-import { onMounted, ref } from "vue";
-import { Line } from "vue-chartjs";
-import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
-  Legend,
-} from "chart.js";
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
-  Legend
-);
-
-interface WeightData {
-  day: string;
-  weight: number;
-}
-
-const loadingDataFinished = ref(false);
-const plotData = ref({
-  labels: ["January", "February", "March", "April", "May", "June", "July"],
-  datasets: [
-    {
-      label: "Data One",
-      backgroundColor: "#f87979",
-      data: [40, 39, 10, 40, 39, 80, 40],
-    },
-  ],
-});
-const plotOptions = ref({
-  responsive: true,
-  maintainAspectRatio: false,
-  tension: 0.5,
-});
-
-onMounted(async () => {
-  const { data, error } = await supabase
-    .from("diary")
-    .select("day,currentWeight")
-    .not("currentWeight", "is", null)
-    .order("day", { ascending: false });
-  if (error === null && data.length !== 0) {
-    console.log(data);
-    // plotData.value.datasets[0].data = data;
-    loadingDataFinished.value = true;
-  } else {
-    console.log(error);
-  }
-});
-</script>
+<script setup lang="ts"></script>
 
 <style scoped></style>
